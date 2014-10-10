bc353389436779f7bd9f551dde0732a4bd954efb
my pc <my pc@mypc-PC>
2014-10-10 10:52:49 +0530

    New class for doing file operations
    Change-Id: Iab531b9ce334cb5ff268fcb3e84c8a4c87dd832e
    Signed-off-by: my pc <my pc@mypc-PC>

---
+++ RamaDeviProjects/src/FileProgram.java
@@ -0,0 +1,12 @@
+
+public class FileProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello How are you doing?");
+
+	}
+
+}
---
+++ Vivek Projects/src/Files.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Files {
+	public static void main(String args[]) throws IOException {
+	
+	
+	}
+
+}
